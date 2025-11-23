f5abd3146a9184f4075c5589ed192054e0093655
npodrascanin <nikola.podrascanin@eng.it>
2025-11-23 21:52:43 +0100

    Init Carter

--- LogisticsInvoiceCalculatorApi/Features/Customers/Create.cs
+++ LogisticsInvoiceCalculatorApi/Features/Customers/Create.cs
@@ -1,6 +1,22 @@
 ﻿namespace LogisticsInvoiceCalculatorApi.Features.Customers;
 
-public sealed class CreateCustomerEndpoint
+public sealed class CreateCustomerEndpoint :
+    ICarterModule
+{
+    public void AddRoutes(
+        IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/customers", async (
+            CreateCustomerRequest request) =>
+            {
+                throw new ArgumentException("In progress");
+            })
+            .WithName("Create Customer")
+            .WithSummary("Create Customer");
+    }
+}
+
+public class CreateCustomerRequest
 {
 
 }
